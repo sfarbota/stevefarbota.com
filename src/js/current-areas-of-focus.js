b1d8c82e2a34a9ac2f85a92d9342340b86688354
root <root@stevefarbota.com>
2016-03-03 02:32:05 -0500

    Improve efficiency of current-areas-of-focus.js by not repeating steps unless necessary.

--- src/js/current-areas-of-focus.js
+++ src/js/current-areas-of-focus.js
@@ -1,9 +1,9 @@
 var stackOverflowUserId = 170309;
 var tagCount = 6;
 var minContainerR = 100;
-var minFractionOfContainerR = 0.15;
-var maxFractionOfContainerR = 0.35;
-var speed = 0.8;
+var minBallPercentageOfContainerR = 0.15;
+var maxBallPercentageOfContainerR = 0.35;
+var speedPercentageOfContainerR = 0.003;
 
 var container = document.getElementById('current-areas-of-focus-container');
 var containerRect = container.getBoundingClientRect();
@@ -11,10 +11,11 @@ var containerParentRect = container.parentElement.getBoundingClientRect();
 var mainFooterRect = $('#main-footer')[0].getBoundingClientRect();
 var containerHeight = mainFooterRect.top - containerParentRect.bottom;
 var containerWidth = containerRect.right - containerRect.left;
-mainContainerR = containerWidth < minContainerR ? containerWidth : minContainerR;
+minContainerR = containerWidth < minContainerR ? containerWidth : minContainerR;
 var containerR = (containerHeight < containerWidth ? containerHeight : containerWidth) / 2;
 containerR = containerR < minContainerR ? minContainerR : containerR;
 
+var speed = speedPercentageOfContainerR * containerR;
 var textSize = containerR / 20;
 var textLineSpacing = containerR / 20;
 
@@ -79,30 +80,34 @@ function getTags() {
           }
           var dominantColorRGB = 'rgb(' + dominantColor.join(',') + ')';
           pushBall(minTagUsageCount, maxTagUsageCount, val.count, dominantColorRGB, imageSource, val.name, tagDescriptions[val.name]);
+          checkIfAllBallsHaveBeenPushed();
         };
       }).fail(function() {
           pushBall(minTagUsageCount, maxTagUsageCount, val.count, randomColor({hue: 'random', luminosity: 'bright', count: 1}), null, val.name, tagDescriptions[val.name]);
+          checkIfAllBallsHaveBeenPushed();
       });
       
       if (key >= tagCount - 1) {
         return false;
       }
     });
-
-    initDrawing();
-    
-    //draw();
-    setInterval(draw, 10);
   });
 }
 
 function pushBall(minTagUsageCount, maxTagUsageCount, curTagUsageCount, color, imageSource, title, description) {
-  var minBallR = minFractionOfContainerR * containerR;
-  var maxBallR = maxFractionOfContainerR * containerR;
+  var minBallR = minBallPercentageOfContainerR * containerR;
+  var maxBallR = maxBallPercentageOfContainerR * containerR;
   var ballRRange = maxBallR - minBallR;
   var tagUsageCountRange = maxTagUsageCount - minTagUsageCount;
   var ballR = (ballRRange * (curTagUsageCount - minTagUsageCount) / tagUsageCountRange) + minBallR;
   balls.push(getBall(containerR * (Math.random() + 0.5), containerR * (Math.random() + 0.5), (Math.random() < 0.5 ? -1 : 1) * speed, (Math.random() < 0.5 ? -1 : 1) * speed, ballR, color, imageSource, title, description));
+}
+
+function checkIfAllBallsHaveBeenPushed() {
+  if (balls.length == tagCount) {
+    initDrawing();
+    setInterval(moveBalls, 10);
+  }
 }
 
 function getBall(xVal, yVal, dxVal, dyVal, rVal, colorVal, imageVal, titleVal, descriptionVal) {
@@ -128,86 +133,100 @@ function initDrawing() {
   svg.style.borderRadius = containerR + 'px';
   svg.style.background = '#eee';
 	svg.style.font = textSize + 'px Arial';
-}
-
-function draw() {
+	
   for (var i = 0; i < balls.length; i++) {
     var curBall = balls[i];
+    
+    var curBallBackground = document.createElementNS(svgNamespace, 'circle');
+    curBallBackground.id = 'ball-' + i;
+    curBallBackground.setAttributeNS(null, 'cx', curBall.x);
+    curBallBackground.setAttributeNS(null, 'cy', curBall.y);
+    curBallBackground.setAttributeNS(null, 'r',  curBall.r);
+    curBallBackground.setAttributeNS(null, 'fill', curBall.color);
+    curBallBackground.style.cursor = 'pointer';
 
+    if (curBall.image !== null) {
+      var curBallImage = document.createElementNS(svgNamespace, 'image');
+      curBallImage.id = 'ball-' + i + '-image';
+      curBallImage.setAttributeNS('http://www.w3.org/1999/xlink', 'href', curBall.image);
+      curBallImage.setAttributeNS(null,'height', curBall.r * 1.4);
+      curBallImage.setAttributeNS(null,'width', curBall.r * 1.4);
+      curBallImage.setAttributeNS(null,'x', curBall.x - (curBall.r * 0.7));
+      curBallImage.setAttributeNS(null,'y', curBall.y - (curBall.r * 0.7));
+      curBallImage.style.cursor = 'pointer';
+      $('#svg-wrapper').append(curBallBackground);
+      $('#svg-wrapper').append(curBallImage);
+    } else {
+			var curBallText = document.createElementNS(svgNamespace, 'text');
+      curBallText.id = 'ball-' + i + '-text';
+      curBallText.setAttributeNS(null,'x', curBall.x);
+      curBallText.setAttributeNS(null,'y', curBall.y);
+      curBallText.setAttributeNS(null, 'fill', '#000');
+      curBallText.style.cursor = 'pointer';
+      $('#svg-wrapper').append(curBallBackground);
+      $('#svg-wrapper').append(curBallText);
+      var titleWords = curBall.title.toUpperCase().split('-');
+      for (var j = 0; j < titleWords.length; j++) {
+        var curTitleWordTextSpan = document.createElementNS(svgNamespace, 'tspan');
+        curTitleWordTextSpan.setAttributeNS(null,'dy', textLineSpacing + 'px');
+        curTitleWordTextSpan.textContent = titleWords[j];
+        $('#ball-' + i + '-text').append(curTitleWordTextSpan);
+        curTitleWordTextSpan.setAttributeNS(null,'x', curBall.x - (curTitleWordTextSpan.getComputedTextLength() / 2));
+      }
+      curBallText.setAttributeNS(null,'x', curBall.x - (curBallText.getComputedTextLength() / 2));
+      curBallText.setAttributeNS(null,'y', curBall.y - (textSize + ((titleWords.length - 1) * textLineSpacing)) / 2);
+    }
+  }
+}
+
+function moveBalls() {
+  for (var i = 0; i < balls.length; i++) {
     if (! $('#ball-' + i).filter(function() { return $(this).is(':hover'); }).length
     		&& ! $('#ball-' + i + '-image').filter(function() { return $(this).is(':hover'); }).length
     		&& ! $('#ball-' + i + '-text').filter(function() { return $(this).is(':hover'); }).length) {
-      $('#ball-' + i + '-text').remove();
-      $('#ball-' + i + '-image').remove();
-      $('#ball-' + i).remove();
-      var circle = document.createElementNS(svgNamespace, 'circle');
-      circle.id = 'ball-' + i;
-      circle.setAttributeNS(null, 'cx', curBall.x);
-      circle.setAttributeNS(null, 'cy', curBall.y);
-      circle.setAttributeNS(null, 'r',  curBall.r);
-      circle.setAttributeNS(null, 'fill', curBall.color);
-      circle.style.cursor = 'pointer';
-
-      if (curBall.image !== null) {
-        var curBallImage = document.createElementNS(svgNamespace, 'image');
-        curBallImage.id = 'ball-' + i + '-image';
-        curBallImage.setAttributeNS('http://www.w3.org/1999/xlink', 'href', curBall.image);
-        curBallImage.setAttributeNS(null,'height', curBall.r * 1.4);
-        curBallImage.setAttributeNS(null,'width', curBall.r * 1.4);
-        curBallImage.setAttributeNS(null,'x', curBall.x - (curBall.r * 0.7));
-        curBallImage.setAttributeNS(null,'y', curBall.y - (curBall.r * 0.7));
-        curBallImage.setAttributeNS(null, 'visibility', 'visible');
-        curBallImage.style.cursor = 'pointer';
-        $('#svg-wrapper').append(circle);
-        $('#svg-wrapper').append(curBallImage);
-      } else {
-  			var curBallText = document.createElementNS(svgNamespace, 'text');
-        curBallText.id = 'ball-' + i + '-text';
-        var titleWords = curBall.title.toUpperCase().split('-');
-        //var baseX = curBall.x;
-        //var baseY = curBall.y + (containerR / 40);
-        curBallText.setAttributeNS(null,'x', curBall.x);
-        curBallText.setAttributeNS(null,'y', curBall.y);
-        //curBallText.textContent = curBall.title.toUpperCase().replace(/-/g, ' ');
-        curBallText.setAttributeNS(null, 'fill', '#000');
-        curBallText.style.cursor = 'pointer';
-        $('#svg-wrapper').append(circle);
-        $('#svg-wrapper').append(curBallText);
-        for (var j = 0; j < titleWords.length; j++) {
-          var curTitleWordTextSpan = document.createElementNS(svgNamespace, 'tspan');
-          curTitleWordTextSpan.setAttributeNS(null,'dy', textLineSpacing + 'px');
-          curTitleWordTextSpan.textContent = titleWords[j];
-          $('#ball-' + i + '-text').append(curTitleWordTextSpan);
-          curTitleWordTextSpan.setAttributeNS(null,'x', curBall.x - (curTitleWordTextSpan.getComputedTextLength() / 2));
-        }
-        //if (curBallText.getComputedTextLength() / 2 > curBall.r) {
-        //  curBallText.textContent = curBallText.textContent.replace(/ /g, '&lt;br/&gt;')
-        //}
-        curBallText.setAttributeNS(null,'x', curBall.x - (curBallText.getComputedTextLength() / 2));
-        curBallText.setAttributeNS(null,'y', curBall.y - (textSize + ((titleWords.length - 1) * textLineSpacing)) / 2);
-      }
-      
-      curBall.x += curBall.dx;
-      curBall.y += curBall.dy;
-      var dx = curBall.x - containerR;
-      var dy = curBall.y - containerR;
-      var distanceFromCenter = Math.sqrt(dx * dx + dy * dy);
-
-      if (distanceFromCenter >= containerR - curBall.r) {
-        var normalMagnitude = distanceFromCenter;
-        var normalX = dx / normalMagnitude;
-        var normalY = dy / normalMagnitude;
-        var tangentX = -normalY;
-        var tangentY = normalX;
-        var normalSpeed = -(normalX * curBall.dx + normalY * curBall.dy);
-        var tangentSpeed = tangentX * curBall.dx + tangentY * curBall.dy;
-        curBall.dx = normalSpeed * normalX + tangentSpeed * tangentX;
-        curBall.dy = normalSpeed * normalY + tangentSpeed * tangentY;
-      }
+		  moveBall(i);
     }
   }
+}
+
+function moveBall(i) {
+  ball = balls[i];
   
-  //requestAnimationFrame(function() {
-  //  draw();
-  //});
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+  
+  var dx = ball.x - containerR;
+  var dy = ball.y - containerR;
+  var distanceFromCenter = Math.sqrt(dx * dx + dy * dy);
+
+  if (distanceFromCenter >= containerR - ball.r) {
+    var normalMagnitude = distanceFromCenter;
+    var normalX = dx / normalMagnitude;
+    var normalY = dy / normalMagnitude;
+    var tangentX = -normalY;
+    var tangentY = normalX;
+    var normalSpeed = -(normalX * ball.dx + normalY * ball.dy);
+    var tangentSpeed = tangentX * ball.dx + tangentY * ball.dy;
+    ball.dx = normalSpeed * normalX + tangentSpeed * tangentX;
+    ball.dy = normalSpeed * normalY + tangentSpeed * tangentY;
+  }
+  
+  var background = $('#ball-' + i)[0];
+  background.setAttributeNS(null, 'cx', ball.x);
+  background.setAttributeNS(null, 'cy', ball.y);
+
+  if (ball.image !== null) {
+    var ballImage = $('#ball-' + i + '-image')[0];
+    ballImage.setAttributeNS(null,'x', ball.x - (ball.r * 0.7));
+    ballImage.setAttributeNS(null,'y', ball.y - (ball.r * 0.7));
+  } else {
+		var ballText = $('#ball-' + i + '-text')[0];
+    var titleWords = ball.title.toUpperCase().split('-');
+    for (var j = 0; j < titleWords.length; j++) {
+		  var curTitleWordTextSpan = ballText.children[j];
+      curTitleWordTextSpan.setAttributeNS(null,'x', ball.x - (curTitleWordTextSpan.getComputedTextLength() / 2));
+    }
+    ballText.setAttributeNS(null,'x', ball.x - (ballText.getComputedTextLength() / 2));
+    ballText.setAttributeNS(null,'y', ball.y - (textSize + ((titleWords.length - 1) * textLineSpacing)) / 2);
+  }
 }
